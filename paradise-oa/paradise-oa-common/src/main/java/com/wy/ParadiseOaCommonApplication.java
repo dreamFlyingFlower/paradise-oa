2ba082812fd5c62959197a4876f06391b924a563
ParadiseWY <582822832@qq.com>
2021-01-12 17:35:31 +0800

    study something

---
+++ paradise-oa/paradise-oa-common/src/main/java/com/wy/ParadiseOaCommonApplication.java
@@ -0,0 +1,13 @@
+package com.wy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParadiseOaCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParadiseOaCommonApplication.class, args);
+	}
+
+}
