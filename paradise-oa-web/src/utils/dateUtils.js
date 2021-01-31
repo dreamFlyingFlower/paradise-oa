06b5c0ec341a91b3c3f98830d2cf792aa9faee24
ParadiseWY <582822832@qq.com>
2021-01-31 21:39:46 +0800

    study something

--- paradise-oa-web/src/utils/dateUtils.js
+++ paradise-oa-web/src/utils/dateUtils.js
@@ -1,4 +1,4 @@
-class dateUtils {
+export default class DateUtils {
   defaultFormat = "yyyy-MM-dd HH:mm:ss";
 
   formatterNumber = formatter => {
@@ -14,64 +14,89 @@ class dateUtils {
 
   }
 
-  static formatDateTime(date = new Date()) {
-    let ymd = formatDate(date);
-    let year = date.getFullYear();
-    let month = date.getMonth() + 1;
-    let day = date.getDate();
-    let hour = date.getHours();
-    let minute = date.getMinutes();
-    let second = date.getSeconds();
-    return [year, month, day].map(this.formatterNumber).join("-") + " "
-        + [hour, minute, second].map(this.formatterNumber).join(":");
+  static formatDateTime (date = new Date()) {
+    return this.formatDate(date) + " " + this.formatTime(date);
   }
 
-  static formatDate(date = new Date()) {
+  static formatDate (date = new Date()) {
     let year = date.getFullYear();
     let month = date.getMonth() + 1;
     let day = date.getDate();
     return [year, month, day].map(this.formatterNumber).join("-");
   }
 
-  static formatTime(date = new Date()) {
+  static formatTime (date = new Date()) {
     let hour = date.getHours();
     let minute = date.getMinutes();
     let second = date.getSeconds();
     return [hour, minute, second].map(this.formatterNumber).join(":");
   }
 
-  static parseDateTime() {
+  static parseDateTime () {
   }
 
-  static parseDateTime(strDate) {
-
-  }
-
-  static parseDateTime(strDate, format) {
+  static parseDateTime (strDate) {
 
   }
 
-  static parseDate() {
+  static parseDateTime (strDate, format) {
+    if (arguments.length === 0) {
+      return null;
+    }
+    const format = format || '{y}-{m}-{d} {h}:{i}:{s}';
+    let date;
+    if (typeof strDate === 'object') {
+      date = strDate;
+    } else {
+      if ((typeof strDate === 'string') && (/^[0-9]+$/.test(strDate))) {
+        strDate = parseInt(strDate);
+      }
+      if ((typeof strDate === 'number') && (strDate.toString().length === 10)) {
+        strDate = strDate * 1000;
+      }
+      date = new Date(strDate);
+    }
+    const formatObj = {
+      y: date.getFullYear(),
+      m: date.getMonth() + 1,
+      d: date.getDate(),
+      h: date.getHours(),
+      i: date.getMinutes(),
+      s: date.getSeconds(),
+      a: date.getDay()
+    }
+    const time_str = format.replace(/{(y|m|d|h|i|s|a)+}/g, (result, key) => {
+      let value = formatObj[key];
+      if (key === 'a') { return ['日', '一', '二', '三', '四', '五', '六'][value]; }
+      if (result.length > 0 && value < 10) {
+        value = '0' + value;
+      }
+      return value || 0;
+    })
+    return time_str;
+  }
+
+  static parseDate () {
 
   }
 
-  static parseDate(strDate) {
+  static parseDate (strDate) {
 
   }
 
-  static parseDate(strDate, format) {
+  static parseDate (strDate, format) {
 
   }
 
-  static parseTime() {
+  static parseTime () {
 
   }
 
-  static parseTime(strDate) {
+  static parseTime (strDate) {
 
   }
 
-  static parseTime(strDate, format) {
+  static parseTime (strDate, format) {
 
   }
 }
--- paradise-oa-web/src/utils/index.js
+++ paradise-oa-web/src/utils/index.js
@@ -1,41 +1,3 @@
-export function parseTime(time, cFormat) {
-    if (arguments.length === 0) {
-        return null
-    }
-    const format = cFormat || '{y}-{m}-{d} {h}:{i}:{s}'
-    let date
-    if (typeof time === 'object') {
-        date = time
-    } else {
-        if ((typeof time === 'string') && (/^[0-9]+$/.test(time))) {
-            time = parseInt(time)
-        }
-        if ((typeof time === 'number') && (time.toString().length === 10)) {
-            time = time * 1000
-        }
-        date = new Date(time)
-    }
-    const formatObj = {
-        y: date.getFullYear(),
-        m: date.getMonth() + 1,
-        d: date.getDate(),
-        h: date.getHours(),
-        i: date.getMinutes(),
-        s: date.getSeconds(),
-        a: date.getDay()
-    }
-    const time_str = format.replace(/{(y|m|d|h|i|s|a)+}/g, (result, key) => {
-        let value = formatObj[key]
-        // Note: getDay() returns 0 on Sunday
-        if (key === 'a') { return ['日', '一', '二', '三', '四', '五', '六'][value ] }
-        if (result.length > 0 && value < 10) {
-            value = '0' + value
-        }
-        return value || 0
-    })
-    return time_str
-}
-
 /**
  * @param {number} time
  * @param {string} option
@@ -337,4 +299,15 @@ export function removeClass(ele, cls) {
         const reg = new RegExp('(\\s|^)' + cls + '(\\s|$)')
         ele.className = ele.className.replace(reg, ' ')
     }
+}
+
+/**
+ * 将一个对象的值赋值到源对象上,并且不覆盖源对象上的同名属性
+ * @param {Object} source 源对象
+ * @param {Object} obj 其他对象
+ */
+export function assign(source,obj){
+    let temp = {...source};
+    Object.assign(source,obj);
+    Object.assign(source,temp);
 }
